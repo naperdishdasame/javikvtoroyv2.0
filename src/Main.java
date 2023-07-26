abe78b5d3d291f969bcc91c3dbadb61897602bdd
naperdish <pihuihush@1hokage.ru>
2023-07-26 20:43:02 +0300

    code

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-     
+     int x = 1000;
+     System.out.println("Начальная сумма на счету клиента:" + x);
+     int y = 1200;
+        System.out.println("Пополнение:" + y);
+int z = y>1000 ? y/100 : 0;
+        System.out.println("Бонусные рубли:" + z);
+        int s = x + z + y;
+        System.out.println("Итоговая сумма на счету клиента: " + s);
+
+    }
+}
